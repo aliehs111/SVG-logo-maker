6a571ea774f76d4174b78f466e589b9de33bf615
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-07-30 13:29:32 -0400

    fixed testing code and fixed bug in dependecies

--- develop/index.js
+++
@@ -1,3 +0,0 @@
-const CLI = require("./lib/cli");
-// import CLI from "../lib/cli";
-new CLI().run();
---
+++ index.js
@@ -0,0 +1,5 @@
+const CLI = require("./lib/cli");
+// import CLI from "./lib/cli";
+// const fetch = require("node-fetch");
+// console.log(fetch);
+new CLI().run();
